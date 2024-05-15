256496a0a848c34ac554d04b24f0495ae3a35517
Jay Sahay <jaysahay78@gmail.com>
2024-05-16 01:21:20 +0530

    avl trees

--- TreeNode.java
+++ TreeNode.java
@@ -1,17 +1,21 @@
 import java.util.*;
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode root;
+public class TreeNode {
+    int val;
+    int height;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode(){}
-        TreeNode(int val){
-            this.val = val;}
-        TreeNode(int val, TreeNode left, TreeNode right){
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    public int getValue() {
+        return val;
     }
+
+    TreeNode(){}
+    TreeNode(int val){
+        this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
