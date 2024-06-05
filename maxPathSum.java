911559ba3a7f2d02d80ef2f2c15340cca2116c05
Jay Sahay <jaysahay78@gmail.com>
2024-06-05 18:25:19 +0530

    files added

---
+++ maxPathSum.java
@@ -0,0 +1,16 @@
+public class maxPathSum {
+    static int MaxSumPath(TreeNode root){
+        int[] max = new int[1];
+        maxPathDown(root, max);
+        return max[0];
+    }
+    static int maxPathDown(TreeNode root, int[] max){
+        if (root == null){
+            return 0;
+        }
+        int left = Math.max(0, maxPathDown(root.left, max));
+        int right = Math.max(0, maxPathDown(root.right, max));
+        max[0] = Math.max(max[0], left+right+root.val);
+        return Math.max(left, right) + root.val;
+    }
+}
