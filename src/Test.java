82dcde7fcbead379b6d2a36e36580049715f6448
Jay Sahay <jaysahay78@gmail.com>
2025-04-29 16:34:22 +0530

    largest odd number in a string

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        String s = "hello ";
-        s = "world" + s;
-        System.out.println(s);
+        int ans = 0;
+        String s = "21";
+        char ch = s.charAt(0);
+        if((ch-'0') % 2 == 0){
+            ans = ch-'0';
+        }
+        System.out.println(ans);
     }
 }
