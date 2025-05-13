11e190557d12b5b0c1140a3fac4238536352613b
Jay Sahay <jaysahay78@gmail.com>
2025-05-13 09:27:07 +0530

    middle node of a linked list

---
+++ src/MiddleNode.java
@@ -0,0 +1,14 @@
+public class MiddleNode {
+    static ListNode middle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+
+    }
+}
