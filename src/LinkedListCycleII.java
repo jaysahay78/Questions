ff07ddfef242f777d9d532cdde9557bd0d8b6ff4
Jay Sahay <jaysahay78@gmail.com>
2025-05-13 20:24:48 +0530

    return starting point of a linked list

---
+++ src/LinkedListCycleII.java
@@ -0,0 +1,23 @@
+public class LinkedListCycleII {
+    static ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
