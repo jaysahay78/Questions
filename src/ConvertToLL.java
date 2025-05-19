9b3d3c5d10fda3f8cf7edb48a9d4e449fa4b292b
Jay Sahay <jaysahay78@gmail.com>
2025-05-19 12:11:03 +0530

    convert array to linked list

---
+++ src/ConvertToLL.java
@@ -0,0 +1,13 @@
+public class ConvertToLL {
+    static ListNode convertArrtoLL(int[] arr) {
+        int n = arr.length;
+        ListNode head= new ListNode(arr[0]);
+        ListNode mover=head;
+        for (int i=1;i<arr.length;i++){
+            ListNode temp= new ListNode(arr[i]);
+            mover.next=temp;
+            mover=mover.next;
+        }
+        return head;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,44 +1,13 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Test {
-    static int testfunc(int n, int[] nums){
-    int candidate = -1, count = 0;
-
-    // Phase 1: Find a potential majority candidate
-    for (int num : nums) {
-        if (count == 0) {
-            candidate = num;
-            count = 1;
-        } else if (num == candidate) {
-            count++;
-        } else {
-            count--;
-        }
+    static int nodeNumber(ListNode head, int n) {
+        ListNode temp = head;
     }
-
-    // Phase 2: Verify the candidate by counting its occurrences
-    count = 0;
-    for (int num : nums) {
-        if (num == candidate) {
-            count++;
-        }
-    }
-
-    // Return the candidate if it's a valid majority element
-    if (count > n / 2) {
-        return candidate;
-    } else {
-        return -1;
-    }
-}
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int nums[] = new int[N];
-        for (int i = 0; i < N; i++) {
-            nums[i] = sc.nextInt();
-        }
-        System.out.println(testfunc(N, nums));
+        Convert
+
     }
 }
