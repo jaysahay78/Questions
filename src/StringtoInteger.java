8bce7feea16ab1cbc01b547dab85ded735b30ac1
Jay Sahay <jaysahay78@gmail.com>
2025-06-05 15:56:04 +0530

    recursive string to integer

--- src/StringtoInteger.java
+++ src/StringtoInteger.java
@@ -1,23 +1,44 @@
 public class StringtoInteger {
     static int myAtoi(String s) {
+//        s=s.trim();
+//        long ans = 0;
+//        int sign = 1;
+//        int i=0;
+//        if (s.isEmpty()) return 0;
+//        if(s.charAt(0) == '-' || s.charAt(0) == '+') {
+//            sign = s.charAt(0) == '+' ? 1 : -1;
+//            i++;
+//        }
+//        while(i<s.length() && Character.isDigit(s.charAt(i))) {
+//            int digit = s.charAt(i) - '0';
+//            ans = (ans*10) + digit;
+//            if(ans*sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+//            if(ans*sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+//            i++;
+//        }
+//
+//        return (int) (ans*sign);
+//    }
         s=s.trim();
-        long ans = 0;
-        int sign = 1;
-        int i=0;
         if (s.isEmpty()) return 0;
-        if(s.charAt(0) == '-' || s.charAt(0) == '+') {
-            sign = s.charAt(0) == '+' ? 1 : -1;
-            i++;
+        int sign = 1;
+        if(s.charAt(0) == '+') {
+            sign = 1;
+            s = s.substring(1);
         }
-        while(i<s.length() && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-            ans = (ans*10) + digit;
-            if(ans*sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if(ans*sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            i++;
+        else if(s.charAt(0) == '-') {
+            sign = -1;
+            s = s.substring(1);
         }
+        return func(s,0, sign);
+    }
 
-        return (int) (ans*sign);
+    static int func(String s, int num, int sign){
+        if(s.length() == 0 || !Character.isDigit(s.charAt(0))) return num*sign;
+        int digit = s.charAt(0) - '0';
+        if (sign == 1 && num > (Integer.MAX_VALUE - digit) / 10) return Integer.MAX_VALUE;
+        if (sign == -1 && -num < (Integer.MIN_VALUE + digit) / 10) return Integer.MIN_VALUE;
+        return func(s.substring(1), num*10 + digit, sign);
     }
 
     public static void main(String[] args) {
--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-
-        ListNode node3 = new ListNode(3,null);
-        ListNode node2 = new ListNode(2,node3);
-        ListNode node1 = new ListNode(1, node2);
-
-        ListNode temp = node1;
-        System.out.println(temp.next.next.val);
+        int n = Integer.MIN_VALUE;
+        System.out.println(n);
     }
 }
