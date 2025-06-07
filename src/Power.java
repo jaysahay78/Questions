007c604167844f8cebd4d4d6127a2f1920dad17a
Jay Sahay <jaysahay78@gmail.com>
2025-06-07 10:37:35 +0530

    power of a number

---
+++ src/Power.java
@@ -0,0 +1,23 @@
+public class Power {
+    static double myPow(double x, int n) {
+        double ans = 1.0;
+        long N = n;
+        if(N<0) N = N*-1;
+        while(N>0){
+            if(N%2 == 1){
+                ans = ans*x;
+                N--;
+            }
+            else{
+                x = x*x;
+                N=N/2;
+            }
+        }
+        if(n<0) ans = (double)1/ (double)ans;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2,-200000000));
+    }
+}
