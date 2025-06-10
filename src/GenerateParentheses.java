34a6f662d5b010d2882985fbb3806ca73711d518
Jay Sahay <jaysahay78@gmail.com>
2025-06-10 23:36:37 +0530

    generate all parentheses

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    static List<String> generateParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        generate(result, 0,0,"", n);
+        return result;
+    }
+    static void generate(List<String> list, int open, int close, String unpr, int n){
+        if(unpr.length() == 2*n){
+            list.add(unpr);
+            return;
+        }
+        if(open < n){
+            generate(list, open+1, close, unpr+'(', n);
+        }
+        if(close < open){
+            generate(list, open, close+1, unpr+')', n);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
