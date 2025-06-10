a466707a85ee39ba7e03815d2fdeb531bdb35cb2
Jay Sahay <jaysahay78@gmail.com>
2025-06-11 00:20:41 +0530

    changes

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -4,21 +4,19 @@ import java.util.List;
 public class GenerateParentheses {
     static List<String> generateParentheses(String s) {
         List<String> result = new ArrayList<>();
-        generate(result, 0,0,"", n);
+        generatePar(result, 0,0,"", s.length());
         return result;
-        int[] arr = {1,2,3,4,5};
-        int a = arr.length;
     }
-    static void generate(List<String> list, int open, int close, String unpr, int n){
+    static void generatePar(List<String> list, int open, int close, String unpr, int n){
         if(unpr.length() == 2*n){
             list.add(unpr);
             return;
         }
         if(open < n){
-            generate(list, open+1, close, unpr+'(', n);
+            generatePar(list, open+1, close, unpr+'(', n);
         }
         if(close < open){
-            generate(list, open, close+1, unpr+')', n);
+            generatePar(list, open, close+1, unpr+')', n);
         }
     }
 
