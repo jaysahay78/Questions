e6fc2948e0d61309cafb59aa910d38a427beb423
Jay Sahay <jaysahay78@gmail.com>
2025-08-13 23:36:48 +0530

    new commit

--- src/Test.java
+++ src/Test.java
@@ -3,13 +3,9 @@ import java.util.LinkedList;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {0,1,0,1,0,1};
-        int l = -2, r=0, count = 0;
-        while(l<arr.length && r<arr.length){
-            if(arr[r] == 1) count++;
-            l++;
-            r++;
-        }
-        System.out.println(count);
+        String s = "bbc";
+        int a = s.charAt(0) - 'a';
+        System.out.println(a);
+        int a  = Math.min(2,3,4);
     }
 }
