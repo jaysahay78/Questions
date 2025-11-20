cdfc63c2d8be6ec6d90b664d6d47c9a9247c4c8c
Jay Sahay <113266635+jaysahay78@users.noreply.github.com>
2025-11-20 12:51:28 +0530

    Time: 6 ms (9.97%) | Memory: 50.3 MB (8.66%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,21 +3,31 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        //tabulation
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int up = Integer.MAX_VALUE, left = Integer.MAX_VALUE;
+                if(i==0 && j==0) dp[0][0] = grid[0][0];
+                else{
+                    if(i>0)up = dp[i-1][j];
+                    if(j>0)left = dp[i][j-1];
+
+                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                }
+            }
         }
-        return func(m-1, n-1, grid, dp);
+        return dp[m-1][n-1];
     }
 
     //memoizaton
-    public int func(int i, int j, int[][] grid, int[][] dp){
-        if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j<0) return Integer.MAX_VALUE;
-        if(dp[i][j] != -1) return dp[i][j];
+    // public int func(int i, int j, int[][] grid, int[][] dp){
+    //     if(i==0 && j==0) return grid[i][j];
+    //     if(i<0 || j<0) return Integer.MAX_VALUE;
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int left = func(i, j-1, grid, dp);
-        int up = func(i-1, j, grid, dp);
+    //     int left = func(i, j-1, grid, dp);
+    //     int up = func(i-1, j, grid, dp);
 
-        return dp[i][j] = grid[i][j] + Math.min(left, up);
-    }
+    //     return dp[i][j] = grid[i][j] + Math.min(left, up);
+    // }
 }
