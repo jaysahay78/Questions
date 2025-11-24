f4cee38ab021cd8119a4194447d29f40b557c799
Jay Sahay <113266635+jaysahay78@users.noreply.github.com>
2025-11-24 17:22:26 +0530

    Time: 3 ms (83.48%) | Memory: 45.5 MB (57.44%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                int same = dp[i+1][j];
+                int next = dp[i+1][j+1];
+
+                dp[i][j] = triangle.get(i).get(j) + Math.min(same, next);
+            }
+        }
+        return dp[0][0];
+    }
+    // public int func(int i, int j, List<List<Integer>> list, int[][] dp){
+    //     if(i== list.size()-1) return list.get(i).get(j);
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     int same = func(i+1, j, list, dp);
+    //     int next = func(i+1, j+1, list, dp);
+    //     return dp[i][j] = list.get(i).get(j) + Math.min(same, next);
+    // }
+}
