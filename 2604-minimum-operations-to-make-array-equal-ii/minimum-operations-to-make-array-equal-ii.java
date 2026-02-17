dbfaca2d39ce8d79f901d0d8372bb11f9135acd6
Jay Sahay <113266635+jaysahay78@users.noreply.github.com>
2026-02-17 16:30:42 +0530

    Time: 4 ms (60.82%) | Memory: 94.9 MB (91.75%) - LeetSync

--- 2604-minimum-operations-to-make-array-equal-ii/minimum-operations-to-make-array-equal-ii.java
+++ 2604-minimum-operations-to-make-array-equal-ii/minimum-operations-to-make-array-equal-ii.java
@@ -3,10 +3,7 @@ class Solution {
         long ops = 0;
         long sum = 0;
         if (k == 0) {
-            for (int i = 0; i < nums1.length; i++) {
-                if (nums1[i] != nums2[i]) return -1;
-            }
-            return 0;
+            return Arrays.equals(nums1, nums2) ? 0 : -1;
         }
         for(int i=0; i<nums1.length; i++){
             if((nums1[i] - nums2[i]) % k != 0)return -1;
